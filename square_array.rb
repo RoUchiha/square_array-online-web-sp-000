17093ef1df9b51c693912a87cc81541ce5d1df43
Roshaan Singh <swaggsikh@gmail.com>
2021-05-07 05:38:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   new_array = []
   
-  array.each do |numbers|
-    new_array << (#{numbers} ** #{numbers})
+  array.each do |number|
+    new_array << (#{number} ** #{number})
   end
   return new_array
 end
